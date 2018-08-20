3b13e2764477741a933e013b4ec9a5c748e6cbd1
SMartQi <375584908@qq.com>
2018-08-20 23:14:24 +0800

    LeetCode 100: Same Tree.

---
+++ Code/0100-same-tree.cc
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if ((p && !q) || (q && !p)) {
+            return false;
+        }
+        if (!p && !q) {
+            return true;
+        }
+        return (p->val == q->val) && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
+};

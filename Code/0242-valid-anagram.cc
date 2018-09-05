48931ba899807d84f4603c92b04ff8b10ededd53
SMartQi <375584908@qq.com>
2018-09-05 23:06:21 +0800

    LeetCode 242: Valid Anagram.

---
+++ Code/0242-valid-anagram.cc
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int a[26] = {0};
+        for (int i = 0; i < s.length(); i++) {
+            a[s[i] - 'a']++;
+            a[t[i] - 'a']--;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (a[i] != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+};

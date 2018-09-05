54701b8f1f48452e9694bd779a7df813d07051c2
SMartQi <375584908@qq.com>
2018-09-05 23:21:49 +0800

    Ugly Number

---
+++ Code/Ugly-Number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isUgly(int num) {
+        if (num < 1) {
+            return false;
+        }
+        if (num == 1) {
+            return true;
+        }
+        while (num % 2 == 0) {
+            num /= 2;
+        }
+        while (num % 3 == 0) {
+            num /= 3;
+        }
+        while (num % 5 == 0) {
+            num /= 5;
+        }
+        return (num == 1);
+    }
+};

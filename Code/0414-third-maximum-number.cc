b152d96add756e9a913cc6ed85578bdf8ee70e64
SMartQi <375584908@qq.com>
2018-09-08 22:31:55 +0800

    LeetCode 414: Third Maximum Number.

---
+++ Code/0414-third-maximum-number.cc
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int thirdMax(vector<int>& nums) {
+        set<int> s;
+        for (int i = 0; i < nums.size(); i++) {
+            s.insert(nums[i]);
+            if (s.size() > 3) {
+                s.erase(s.begin());
+            }
+        }
+        if (s.size() == 3) {
+            return *(s.begin()); 
+        } else {
+            return *(--s.end());
+        }
+    }
+};

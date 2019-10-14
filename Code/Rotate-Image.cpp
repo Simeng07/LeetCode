6d250a77142e795a9be31ac230ce5ac27ce23359
SMartQi <375584908@qq.com>
2019-10-14 22:51:46 +0800

    Rotate Image

---
+++ Code/Rotate-Image.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>> &matrix) {
+        // a_ij -> a_j(n-i-1)
+        int n = matrix.size();
+        for (int i = 0; i < n / 2; i++) {
+            for (int j = i; j < n - 1 - i; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[n-j-1][i];
+                matrix[n-j-1][i] = matrix[n-i-1][n-j-1];
+                matrix[n-i-1][n-j-1] = matrix[j][n-i-1];
+                matrix[j][n-i-1] = tmp;
+            }
+        }
+    }
+};

c9d884e5421d46b965a79e7d99718616e9dd03f1
SMartQi <375584908@qq.com>
2019-11-04 23:18:23 +0800

    LeetCode 236: Lowest Common Ancestor of a Binary Tree.

---
+++ Code/0236-lowest-common-ancestor-of-a-binary-tree.cc
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (root == p || root == q || root == NULL) return root;
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+        return (left != NULL && right != NULL) ? root : (left != NULL ? left : right);
+    }
+};

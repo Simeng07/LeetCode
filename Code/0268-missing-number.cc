2092d9987149f316329b8f3bfcac3d7f6e74047c
SMartQi <375584908@qq.com>
2019-11-07 20:32:19 +0800

    LeetCode 268: Missing Number.

---
+++ Code/0268-missing-number.cc
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int result = 0;
+        // for (int i = 0; i < nums.size(); i++) {
+        //     result += i;
+        //     result -= nums[i];
+        // }
+        // result += nums.size();
+        for (int i = 0; i < nums.size(); i++) {
+            result ^= nums[i];
+            result ^= i;
+        }
+        result ^= nums.size();
+        return result;
+    }
+};

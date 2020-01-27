eca648fd2698dbb119dff418af5179d4c7e64440
SMartQi <375584908@qq.com>
2020-01-27 14:50:08 +0800

    LeetCode 1456: Find the City With the Smallest Number of Neighbors at a Threshold Distance.

--- Code/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cc
+++ Code/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cc
@@ -1,12 +1,10 @@
 class Solution {
-    int result[100][100];
-    int cities[100][100];
 public:
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+        int cities[100][100];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                result[i][j] = -1;
-                cities[i][j] = -1;
+                cities[i][j] = (i == j ? 0 : 10001); // threshold + 1
             }
         }
         
@@ -15,49 +13,32 @@ public:
             cities[edge[1]][edge[0]] = edge[2];
         }
         
-        for (int i = 0; i < n; i++) {
-            helper(n, i, distanceThreshold);
-        }
-        
-        int solutionNode = 0;
-        int solutionCount = INT_MAX;
-        for (int i = 0; i < n; i++) {
-            int tmp = 0;
-            for (int j = 0; j < n; j++) {
-                if (result[i][j] >= 0) {
-                    tmp++;
+        // Floyd Warshall
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    cities[i][j] = min(cities[i][j], cities[i][k] + cities[k][j]);
+                    cities[j][i] = cities[i][j];
                 }
             }
-            if (tmp < solutionCount) {
-                solutionNode = i;
-                solutionCount = tmp;
+        }
+        
+        int resultNode = 0;
+        int resultCount = INT_MAX;
+        
+        for (int i = 0; i < n; i++) {
+            int tmpCount = 0;
+            for (int j = 0; j < n; j++) {
+                if (cities[i][j] <= distanceThreshold) {
+                    tmpCount++;
+                }
             }
-            if (tmp == solutionCount) {
-                solutionNode = max(solutionNode, i);
+            if (tmpCount <= resultCount) {
+                resultNode = i;
+                resultCount = tmpCount;
             }
         }
         
-        return solutionNode;
-    }
-    
-    void helper(int n,int current, int distanceThreshold) {
-        for (int i = 0; i < n; i++) {
-            if (i != current && cities[current][i] > 0 && distanceThreshold - cities[current][i] >= 0) {
-                subHelper(n, i, distanceThreshold - cities[current][i], current, current);
-            }
-        }
-    }
-    
-    void subHelper(int n, int current, int remain, int last, int origin) {
-        result[origin][current] = remain;
-        for (int i = 0; i < n; i++) {
-            if (i != origin 
-                && i != last 
-                && i != current
-                && cities[current][i] > 0
-                && remain - cities[current][i] > result[origin][i]) {
-                subHelper(n, i, remain - cities[current][i], i, origin);
-            }
-        }
+        return resultNode;
     }
 };

4aba375242a32c69223ffdac7550b488ca31dc6f
SMartQi <375584908@qq.com>
2020-01-27 21:09:45 +0800

    LeetCode 1255: Reverse Subarray To Maximize Array Value.

--- Code/1255-reverse-subarray-to-maximize-array-value.cc
+++ Code/1255-reverse-subarray-to-maximize-array-value.cc
@@ -8,16 +8,18 @@ public:
         int smallestBigger = max(nums[0], nums[1]);
         int biggestSmaller = min(nums[0], nums[1]);
         int origin = smallestBigger - biggestSmaller;
+        int diff = 0;
         for (int i = 1; i < nums.size() - 1; i++) {
+            // the origin value
+            origin += abs(nums[i] - nums[i + 1]);
+            // reverse inner part
             smallestBigger = min(smallestBigger, max(nums[i], nums[i + 1])); // smallest bigger one
             biggestSmaller = max(biggestSmaller, min(nums[i], nums[i + 1])); // biggest smaller one
-            origin += abs(nums[i] - nums[i + 1]);
-        }
-        int diff = max(0, 2 * (biggestSmaller - smallestBigger));
-        for (int i = 1; i < nums.size() - 1; i++) {
+            // reverse edge part
             diff = max(diff, abs(nums[i + 1] - nums[0]) - abs(nums[i + 1] - nums[i]));
             diff = max(diff, abs(nums[size - 1] - nums[i]) - abs(nums[i + 1] - nums[i]));
         }
+        diff = max(diff, 2 * (biggestSmaller - smallestBigger));
         return diff + origin;
     }
 };

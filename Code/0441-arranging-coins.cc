4c34b0feb9d2cf1880b615157b96cd2ea65370de
SMartQi <375584908@qq.com>
2020-02-05 21:52:46 +0800

    LeetCode 441: Arranging Coins.

--- Code/0441-arranging-coins.cc
+++ Code/0441-arranging-coins.cc
@@ -1,14 +1,22 @@
 class Solution {
 public:
     int arrangeCoins(int n) {
-        if (n == 0) {
-            return 0;
+        if (n <= 1) {
+            return n;
         }
-        int row = 1;
-        while (n - row >= 0) {
-            n -= row;
-            row++;
+        
+        int low = 0, high = n;
+        while (low < high) {
+            long long mid = (high + low) / 2;
+            long long sum = mid * (mid + 1) / 2;
+            if (sum == n) {
+                return mid;
+            } else if (sum < n) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
         }
-        return row - 1;
+        return low - 1;
     }
 };

76e2cd93aeda803de30235f5059bba4e77fb73d1
SMartQi <375584908@qq.com>
2020-07-05 14:07:59 +0800

    LeetCode 1626: Can Make Arithmetic Progression From Sequence.

---
+++ Code/1626-can-make-arithmetic-progression-from-sequence.cc
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canMakeArithmeticProgression(vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+        int gap = arr[1] - arr[0];
+        for (int i = 2; i < arr.size(); i++) {
+            if (arr[i] - arr[i - 1] != gap) {
+                return false;
+            }
+        }
+        return true;
+    }
+};

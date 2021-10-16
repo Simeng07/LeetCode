aa8417e5fbd16d32a9ed52325ce7def256c74dd1
SMartQi <375584908@qq.com>
2021-10-16 22:31:56 +0800

    LeetCode 2148: Minimum Number of Moves to Seat Everyone.

---
+++ Code/2148-minimum-number-of-moves-to-seat-everyone.cc
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minMovesToSeat(vector<int>& seats, vector<int>& students) {
+        sort(seats.begin(), seats.end());
+        sort(students.begin(), students.end());
+        int result = 0;
+        for (int i = 0; i < seats.size(); i++) {
+            result += abs(seats[i] - students[i]);
+        }
+        return result;
+    }
+};
